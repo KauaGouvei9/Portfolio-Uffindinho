6ab8375918a004ee25fd289fd6bc7b62d93693af
KauaGouvei9 <kaua04gouveia@gmail.com>
2026-09-10 16:55:00 -0300

    Fix: Bug vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // base precisa bater com o nome do repositorio no GitHub Pages:
 // https://<usuario>.github.io/portfolio-uffindinho/
 export default defineConfig({
-  base: '/portfolio-uffindinho/',
+  base: '/Portfolio-Uffindinho/',
   plugins: [react()],
 })
