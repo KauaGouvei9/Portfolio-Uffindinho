26980a53ee89fe2f0218d314bd47231cc3b08ed7
KauaGouvei9 <kaua04gouveia@gmail.com>
2026-09-24 01:47:02 -0300

    Add: link integrante.

--- src/data/equipe.js
+++ src/data/equipe.js
@@ -2,7 +2,7 @@
 // Troque os href "#" pelos perfis reais quando o grupo quiser divulgar.
 
 export const equipe = [
-  { nome: 'Kauã Gouveia de Carvalho', curso: 'Sistema de Informação', github: '#', linkedin: '#' },
+  { nome: 'Kauã Gouveia de Carvalho', curso: 'Sistema de Informação', github: 'https://github.com/KauaGouvei9', linkedin: 'https://www.linkedin.com/in/kau%C3%A3-gouveia-de-carvalho-a98aa331a' },
   { nome: 'Fabricio de Freitas Rivas', curso: 'Sistema de Informação', github: '#', linkedin: '#' },
   { nome: 'Kauã Muller Campista', curso: 'Sistema de Informação', github: '#', linkedin: '#' },
   { nome: 'Kayo Vianna Cipriano', curso: 'Sistema de Informação', github: '#', linkedin: '#' },
